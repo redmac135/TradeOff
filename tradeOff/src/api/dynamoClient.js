3f79a3e0d8ca05ddb4a7960902905a121efbaece
Ethan Zhao <ethanzhao1981@gmail.com>
2025-09-13 23:45:34 -0400

    complete game data hooks

---
+++ tradeOff/src/api/dynamoClient.js
@@ -0,0 +1,12 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({
+  region: "us-east-2",
+  credentials: {
+    accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
+  },
+});
+
+export const ddb = DynamoDBDocumentClient.from(client);
